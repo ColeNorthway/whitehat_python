5dcd50c8831c1154c6ea705014d6a874c5005654
[Cole Northway] <[cenorthway289@gmail.com]>
2025-08-31 23:19:30 -0500

    netcat draft 3

--- netcat.py
+++ netcat.py
@@ -34,89 +34,89 @@ class NetCat:
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 
-        def run(self):
-            if self.args.listen:
-                self.listen()
-            else:
-                self.send()
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
 
-                
-        def send(self):
-            self.socket.connect((self.args.target, self.args.port))
-            if self.buffer:
-                self.socket.send(self.buffer)
+            
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
 
-            try:
-                while True:
-                    recv_len = 1
-                    response = ''
-
-                    while recv_len:
-                        data = self.socket.recv(4096)
-                        recv_len = len(data)
-                        response += data.decode()
-                        if recv_len < 4096:
-                            break
-
-                    if response: #you could prob make this loop exit on buffer = exit
-                        print(response)
-                        buffer = input('> ')
-                        self.socket.send(buffer.encode())
-
-            except KeyboardInterrupt:
-                print('user termed conn')
-                self.socket.close()
-                sys.exit()
-
-
-        def listen(self):
-            self.socket.bind((self.args.target, self.args.port))
-            self.socket.listen(5)
-
+        try:
             while True:
-                client_socket, _ = self.socket.accept()
-                client_thread = threading.Thread(target=self.handle, args=(client_socket,))
-                client_thread.start()
+                recv_len = 1
+                response = ''
 
-
-        def handle(self, client_socket):
-            if self.args.execute:
-                output = execute(self.args.execute)
-                client_socket.send(output.encode())
-            elif self.args.upload:
-                file_buffer = b''
-
-                while True:
-                    data = client_socket.recv(4096)
-                    if data:
-                        file_buffer += data
-                    else:
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
                         break
 
-                with open(self.args.upload, 'wb') as f:
-                    f.write(file_buffer)
+                if response: #you could prob make this loop exit on buffer = exit
+                    print(response)
+                    buffer = input('> ')
+                    self.socket.send(buffer.encode())
 
-                    message = f'Saved file {self.args.upload}'
-                    client_socket.send(message.encode())
-            elif self.args.command:
-                cmd_buffer = b''
-                recv_len = 1
-                while True:
-                    try:
-                        client_socket.send(b'BHP: #> ')
-                        while recv_len:
-                            data = client_socket.recv(64)
-                            recv_len = len(data)
-                            cmd_buffer += data
-                        response = execute(cmd_buffer.decode())
+        except KeyboardInterrupt:
+            print('user termed conn')
+            self.socket.close()
+            sys.exit()
 
-                        if response:
-                            client_socket.send(response.encode())
-                        cmd_buffer = b''
-                    except Exception as e:
-                        print(f'server killed {e}')
-                        self.socket.close()
-                        sys.exit()
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+            client_thread.start()
+
+
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+        elif self.args.upload:
+            file_buffer = b''
+
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+
+                message = f'Saved file {self.args.upload}'
+                client_socket.send(message.encode())
+        elif self.args.command:
+            cmd_buffer = b''
+            recv_len = 1
+            while True:
+                try:
+                    client_socket.send(b'BHP: #> ')
+                    while recv_len:
+                        data = client_socket.recv(64)
+                        recv_len = len(data)
+                        cmd_buffer += data
+                    response = execute(cmd_buffer.decode())
+
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
 
 
 
